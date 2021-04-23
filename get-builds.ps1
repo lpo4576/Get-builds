345e06068fcb894576fd4021722fb720978d19f5
Larkin <55812754+lpo4576@users.noreply.github.com>
2021-04-23 13:15:55 -0500

    Add files via upload

---
+++ get-builds.ps1
@@ -0,0 +1,21 @@
+﻿#Declare variables
+$computers = (Import-Csv -Path C:\pcs\stlpcs2.csv)
+[pscustomobject[]]$finalresults = $null
+$outputpath = "C:\temp\PCBuilds.csv"
+
+Write-host ""
+Write-host "Retrieving build numbers..." -ForegroundColor Yellow
+
+#Grabs build number from registry of each PC and places in array
+foreach ($pc in $computers) {
+    $pcbuild = Invoke-Command -ComputerName $pc.hostname -ScriptBlock {(Get-ItemProperty -Path "HKLM:\software\Microsoft\windows NT\currentversion" -Name Releaseid).releaseid} -ErrorAction SilentlyContinue
+    $results = '' | select Hostname,Build,Location
+    $results.Hostname = $pc.Hostname
+    $results.Build = $pcbuild
+    $results.Location = $pc.Location
+    $finalresults += $results
+    }
+
+$finalresults | Export-Csv -Path $outputpath
+Write-host "Output placed in $outputpath" -ForegroundColor Green
+pause
